53f710c326207841f41e9d0a3fb15add2f9ccc7c
dipesh <dipeshsth00@gmail.com>
2025-02-28 10:37:51 +0545

    update by add date

--- 5th_chapter_lab/CheckIfModified.java
+++ 5th_chapter_lab/CheckIfModified.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.net.*;
+import java.util.Date;
 
 public class CheckIfModified {
     public static void main(String[] args) {
@@ -12,17 +13,16 @@ public class CheckIfModified {
 
             //Set the "ifModifiedSince" time to 24 hours ago
             long lastChecked = System.currentTimeMillis() - (24 * 60 * 60 * 1000); //24 HOURS AGO
-            connection.setIfModifiedSince(lastChecked);
 
             //Get the value set
-            System.out.println("If-Modified-Since; " + connection.getIfModifiedSince());
+            System.out.println("If-Modified-Since; " + new Date(connection.getIfModifiedSince()));
 
             // Connect to the server
             connection.connect();
 
             // Get the last modified date to the resourc
             long lastModified = connection.getLastModified();
-            System.out.println("Last Modified: " + lastModified);
+            System.out.println("Last Modified: " + new Date( lastModified));
 
             if(lastModified == 0){
                 System.out.println("The server did not provide a Last-Modified header.");
